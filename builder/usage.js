db707b82fc090f2e210c22ee4ea8ebc1ca51020b
Rodrigo Jiménez <rosec@protonmail.ch>
2015-04-13 18:43:05 -0500

    desmenuzado

--- builder/usage.js
+++ builder/usage.js
@@ -5,27 +5,31 @@ var afinacion8Factory = require('./afinacion8Cilindros');
 
 
 var tallerDeAbraham = tallerFactory();
-
-var paquete4 = tallerDeAbraham.construir(afinacion4Factory({
+var estrategia4Cilindros = afinacion4Factory({
     bujia: 10,
     filtroAire: 12,
     filtroGasolina: 15,
     litroAceite: 18
-}));
+});
 
-var paquete6 = tallerDeAbraham.construir(afinacion6Factory({
+var estrategia6Cilindros = afinacion6Factory({
     bujia: 12,
     filtroAire: 30,
     filtroGasolina: 45,
     litroAceite: 18
-}));
+});
 
-var paquete8 = tallerDeAbraham.construir(afinacion8Factory({
+var estrategia8Cilindros = afinacion8Factory({
     bujia: 15,
     filtroAire: 60,
     filtroGasolina: 75,
     litroAceite: 18
-}));
+});
+
+
+var paquete4 = tallerDeAbraham.construir(estrategia4Cilindros);
+var paquete6 = tallerDeAbraham.construir(estrategia6Cilindros);
+var paquete8 = tallerDeAbraham.construir(estrategia8Cilindros);
 
 
 console.log('El paquete de 4 bujías cuesta: ' + paquete4.obtenerPresupuesto());
